16e572fa7c3cf6cdf75d79b67e916965a82c5ff6
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-09-05 08:49:29 +0200

    routes basic setup skeleton started

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const router = express.Router();
+
+
+router.get('/item', (req, res, next) => {
+});
+
+
+router.post('/list', (req, res, next) => {
+});
+
+
+router.delete('/list/:id', (req, res, next) => {
+})
+module.exports = router;
