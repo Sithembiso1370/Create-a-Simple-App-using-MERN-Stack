038b46c64752ad60e5bb27d185a9f2991908b897
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-09-05 09:42:45 +0200

    CREATE MONGODB DB CONNECTION

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
 require('dotenv').config();
+const path = require('path');
 
 // Create an instance of the express framework
 const app = express();
@@ -8,6 +12,14 @@ const app = express();
 // Grab the port variable  from the environment variables
 const port = process.env.PORT || 5000;
 
+//connect to the database
+mongoose.connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => console.log(`Database connected successfully`))
+  .catch(err => console.log(err));
+
+//since mongoose promise is depreciated, we overide it with node's promise
+mongoose.Promise = global.Promise;
+
 // Handle CORS related issues
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,9 +27,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.send('Welcome to Express Server');
+
+app.use('/api', routes);
+
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
 });
 
 
