9294aa05dfb6de285c8d2def751157eb6b45c4e0
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-09-05 08:48:54 +0200

    server setup

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 
-
+// Create an instance of the express framework
 const app = express();
 
+
+// Grab the port variable  from the environment variables
 const port = process.env.PORT || 5000;
 
 // Handle CORS related issues
@@ -12,6 +14,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 
 app.use((req, res, next) => {
   res.send('Welcome to Express Server');
